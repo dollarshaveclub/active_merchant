c4fcbc64e434684e58180f08a3f57ddf0aebaf61
Asif Ahmed <asif.h.ahmed@gmail.com>
2017-05-04 13:35:11 -0700

    Tokenization Adyen

--- lib/active_merchant/billing/gateways/adyen.rb
+++ lib/active_merchant/billing/gateways/adyen.rb
@@ -7,6 +7,10 @@ module ActiveMerchant #:nodoc:
       self.test_url = 'https://pal-test.adyen.com/pal/servlet/Payment/v18'
       self.live_url = 'https://pal-live.adyen.com/pal/servlet/Payment/v18'
 
+      class_attribute :test_token_url, :live_token_url
+      self.test_token_url = 'https://pal-test.adyen.com/pal/servlet/Recurring/v18'
+      self.live_token_url = 'https://pal-test.adyen.com/pal/servlet/Recurring/v18'
+
       self.supported_countries = ['AD','AE','AF','AG','AI','AL','AM','AO','AQ','AR','AS','AT','AU','AW','AX','AZ','BA','BB','BD','BE','BF','BG','BH','BI','BJ','BL','BM','BN','BO','BQ','BR','BS','BT','BV','BW','BY','BZ','CA','CC','CD','CF','CG','CH','CI','CK','CL','CM','CN','CO','CR','CU','CV','CW','CX','CY','CZ','DE','DJ','DK','DM','DO','DZ','EC','EE','EG','EH','ER','ES','ET','FI','FJ','FK','FM','FO','FR','GA','GB','GD','GE','GF','GG','GH','GI','GL','GM','GN','GP','GQ','GR','GS','GT','GU','GW','GY','HK','HM','HN','HR','HT','HU','ID','IE','IL','IM','IN','IO','IQ','IR','IS','IT','JE','JM','JO','JP','KE','KG','KH','KI','KM','KN','KP','KR','KW','KY','KZ','LA','LB','LC','LI','LK','LR','LS','LT','LU','LV','LY','MA','MC','MD','ME','MF','MG','MH','MK','ML','MM','MN','MO','MP','MQ','MR','MS','MT','MU','MV','MW','MX','MY','MZ','NA','NC','NE','NF','NG','NI','NL','NO','NP','NR','NU','NZ','OM','PA','PE','PF','PG','PH','PK','PL','PM','PN','PR','PS','PT','PW','PY','QA','RE','RO','RS','RU','RW','SA','SB','SC','SD','SE','SG','SH','SI','SJ','SK','SL','SM','SN','SO','SR','SS','ST','SV','SX','SY','SZ','TC','TD','TF','TG','TH','TJ','TK','TL','TM','TN','TO','TR','TT','TV','TW','TZ','UA','UG','UM','US','UY','UZ','VA','VC','VE','VG','VI','VN','VU','WF','WS','YE','YT','ZA','ZM','ZW']
       self.default_currency = 'USD'
       self.supported_cardtypes = [:visa, :master, :american_express, :diners_club, :jcb, :dankort, :maestro,  :discover]
@@ -30,6 +34,17 @@ module ActiveMerchant #:nodoc:
         requires!(options, :username, :password, :merchant_account)
         @username, @password, @merchant_account = options.values_at(:username, :password, :merchant_account)
         super
+      end
+
+      def store(payment, options = {})
+        requires!(options, :shopper_reference)
+        requires!(options, :recurring)
+        post = init_post(options)
+        add_payment(post, payment)
+        add_extra_data(post, options)
+        add_recurring_data(post, options)
+        url = (test? ? test_token_url : live_token_url)
+        commit('storeToken', post, url)
       end
 
       def purchase(money, payment, options={})
@@ -100,6 +115,10 @@ module ActiveMerchant #:nodoc:
         post[:shopperInteraction] = options[:shopper_interaction] if options[:shopper_interaction]
       end
 
+      def add_recurring_data(post, options)
+        post[:recurring] = options[:recurring]
+      end
+
       def add_address(post, options)
         return unless post[:card] && post[:card].kind_of?(Hash)
         if address = options[:billing_address] || options[:address]
@@ -153,9 +172,7 @@ module ActiveMerchant #:nodoc:
         JSON.parse(body)
       end
 
-      def commit(action, parameters)
-        url = (test? ? test_url : live_url)
-
+      def commit(action, parameters, url = default_url)
         begin
           raw_response = ssl_post("#{url}/#{action.to_s}", post_data(action, parameters), request_headers)
           response = parse(raw_response)
@@ -176,6 +193,10 @@ module ActiveMerchant #:nodoc:
 
       end
 
+      def default_url
+        (test? ? test_url : live_url)
+      end
+
       def basic_auth
         Base64.strict_encode64("#{@username}:#{@password}")
       end
@@ -193,6 +214,8 @@ module ActiveMerchant #:nodoc:
           ['Authorised', 'Received', 'RedirectShopper'].include?(response['resultCode'])
         when 'capture', 'refund', 'cancel'
           response['response'] == "[#{action}-received]"
+        when 'storeToken'
+          response['result'] == 'Success'
         else
           false
         end
