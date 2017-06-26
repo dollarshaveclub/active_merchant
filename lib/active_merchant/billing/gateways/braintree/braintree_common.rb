60b537c2a3442b9a2ef7cf719facfa52b8e0bf3f
Shawntonio Banderas <carrickshawn@gmail.com>
2017-06-26 15:36:14 -0700

    debug statements

--- lib/active_merchant/billing/gateways/braintree/braintree_common.rb
+++ lib/active_merchant/billing/gateways/braintree/braintree_common.rb
@@ -1,6 +1,9 @@
 module BraintreeCommon
   def self.included(base)
     base.supported_countries = ['US', 'AU']
+    puts "TREES"
+    puts base.supported_countries[0]
+    puts base.supported_countries[1]
     base.supported_cardtypes = [:visa, :master, :american_express, :discover, :jcb, :diners_club]
     base.homepage_url = 'http://www.braintreepaymentsolutions.com'
     base.display_name = 'Braintree'
